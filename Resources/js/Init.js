e2257ee95f407d9927559f255eefa0cb06bfb2df
Lukáš Chylík <lukas.chylik@riganti.cz>
2020-09-17 19:56:48 +0200

    some changes

--- Resources/js/Init.js
+++ Resources/js/Init.js
@@ -1,89 +1,76 @@
-﻿$(document).ready(function () {
-    $("#nav-icon4").click(function () {
-        if (!$(".menu").hasClass('showed')) {
-            $(".menu").addClass('showed');
-        }
-        else {
-            $('.menu').removeClass('showed');
-        }
+﻿$(document).ready(function() {
+    $("#nav-icon4").click(function() {
+        $("#menu").toggleClass('showed');
     });
 
-    $(document).click(function (e) {
-        if ($(window).width() < 982 && !$(e.target).hasID('show-nav')) {
-            $('.menu').removeClass('showed');
-        }
-    });
-
-    $('li').click(function () {
+    $('li').click(function() {
         $("li.active").removeClass("active");
         $(this).addClass('active');
     });
 
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(document).scrollTop() > 100) {
             $(".navigation-bar").addClass("small");
         } else {
             $(".navigation-bar").removeClass("small");
         }
     });
-    $("#homeScroll").click(function () {
+    $("#homeScroll").click(function() {
         $('html,body').animate({
             scrollTop: $(".banner-section").offset().top
         }, 'slow');
     });
 
-    $(".footer-content__logo").click(function () {
+    $(".footer-content__logo").click(function() {
         $('html,body').animate({
             scrollTop: $(".banner-section").offset().top
         }, 'slow');
     });
 
-    $("#aboutScroll").click(function () {
+    $("#aboutScroll").click(function() {
         $('html,body').animate({
             scrollTop: $(".about-section").offset().top - 70
         }, 'slow');
     });
 
-    $("#productsScroll").click(function () {
+    $("#productsScroll").click(function() {
         $('html,body').animate({
             scrollTop: $(".products-section").offset().top - 70
         }, 'slow');
     });
 
-    $("#projectsScroll").click(function () {
+    $("#projectsScroll").click(function() {
         $('html,body').animate({
             scrollTop: $(".smallbanner-section").offset().top - 70
         }, 'slow');
     });
 
-    $("#contactScroll").click(function () {
+    $("#contactScroll").click(function() {
         $('html,body').animate({
             scrollTop: $(".contact-section").offset().top - 70
         }, 'slow');
     });
 
-    $('#nav-icon4').click(function () {
+    $('#nav-icon4').click(function() {
         $(this).toggleClass('open');
     });
 
-    $('.prod1').click(function () {
+    $('.prod1').click(function() {
         $('.contact-form__message').val('Dobrý den, rád bych poptal exteriérové práce, konkrétně:');
         $('#athleteBody1').slideUp("slow");
     });
 
-    $('.prod2').click(function () {
+    $('.prod2').click(function() {
         $('.contact-form__message').val('Dobrý den, rád bych poptal interiérové práce, konkrétně');
         $('#athleteBody1').slideUp("slow");
     });
 
-    $('.prod3').click(function () {
+    $('.prod3').click(function() {
         $('.contact-form__message').val('Dobrý den, rád bych poptal výstavbu či rekonstrukci střešních systémů, moje představa je');
         $('#athleteBody1').slideUp("slow");
     });
 });
 
-window.onload = function (e) {
+window.onload = function(e) {
     if (document.location.hash) $(document).scrollTop(document.body.scrollTop - 70);
 }
-
-
